7ebedca453f31d91cfb100069b6a405f5d3dc885
Junyeong-An <2117ab@gmail.com>
2024-06-30 02:54:17 +0900

    Feat: 카테고리 전체 조회 메서드 추가

--- Estrella/src/main/java/org/estrella/cocktail/application/CocktailService.java
+++ Estrella/src/main/java/org/estrella/cocktail/application/CocktailService.java
@@ -61,9 +61,6 @@ public class CocktailService {
                 .limit(n)
                 .collect(Collectors.toList());
     }
-    public List<String> getAllCategories() {
-        return cocktailRepository.findAllCategories();
-    }
 
 
 }
