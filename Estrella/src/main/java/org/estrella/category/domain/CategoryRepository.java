4a97fae5801fee8096c1a74c3ee77a98dc60decc
Junyeong-An <2117ab@gmail.com>
2024-06-30 02:52:59 +0900

    Feat: CategoryRepository 추가

--- Estrella/src/main/java/org/estrella/category/domain/CategoryRepository.java
+++ Estrella/src/main/java/org/estrella/category/domain/CategoryRepository.java
@@ -1,6 +1,12 @@
 package org.estrella.category.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    @Query("SELECT c.name FROM Category c")
+    List<String> findAllCategoryNames();
+
 }
