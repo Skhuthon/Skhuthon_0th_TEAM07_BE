248ddcee8e3ce8c5fd9d91c1b21e18f1508fe522
Junyeong-An <2117ab@gmail.com>
2024-06-30 02:58:18 +0900

    Feat: CategoryController 추가

---
+++ Estrella/src/main/java/org/estrella/category/api/CategoryController.java
@@ -0,0 +1,52 @@
+package org.estrella.category.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.estrella.category.application.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cocktails/categories")
+@Tag(name = "CategoryController", description = "CategoryController API 목록")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    @Operation(
+            summary = "모든 칵테일 카테고리 조회",
+            description = "모든 칵테일 카테고리를 조회합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = List.class),
+                                    examples = @ExampleObject(
+                                            name = "예제 데이터",
+                                            value = "[\"All Day Type Cocktail\", \"Before Dinner Cocktail\", \"After Dinner Cocktail\"]"
+                                    )
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<List<String>> getAllCategoryNames() {
+        List<String> categoryNames = categoryService.getAllCategoryNames();
+        return new ResponseEntity<>(categoryNames, HttpStatus.OK);
+    }
+}
